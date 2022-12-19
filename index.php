af4709538de1b92e40cf55693cac355f78f951d7
M4jdza <lazarvukadinovic7@gmail.com>
2022-12-19 16:49:21 +0100

    update

--- index.php
+++ index.php
@@ -25,11 +25,6 @@
                         <p class="text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium accusamus quisquam adipisci.</p>
                     </div>
                 </div>
-                <!-- <div class="row">
-                    <div class="col">
-                        <a href="#" class="btn btn-success">Kupi knjige</a>
-                    </div>
-                </div> -->
             </div>
         </div>
         <div class="container mt-4">
@@ -41,17 +36,17 @@
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas eveniet incidunt error eum, sed exercitationem aliquid, laboriosam enim odio ad consequuntur consequatur. Incidunt nemo quibusdam, cum possimus soluta distinctio ratione.</p>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 najpoznatije">
                     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                         <div class="carousel-inner">
-                            <div class="carousel-item active slider-image" data-bs-interval="10000">
-                            <img src="./images/server/knjige/hari poter i relikvije smrti.jpg" class="d-block w-100" >
+                            <div class="carousel-item active slider-image" data-bs-interval="4000">
+                                <img src="./images/server/knjige/hari poter i relikvije smrti.jpg" class="d-block w-100" >
                             </div>
-                            <div class="carousel-item slider-image" data-bs-interval="2000">
-                            <img src="./images/server/knjige/zive slike.jpg" class="d-block w-100">
+                            <div class="carousel-item slider-image" data-bs-interval="4000">
+                                <img src="./images/server/knjige/zive slike.jpg" class="d-block w-100">
                             </div>
                             <div class="carousel-item slider-image">
-                            <img src="./images/server/knjige/kljuc besmrtnosti.jpg" class="d-block w-100" >
+                                <img src="./images/server/knjige/kljuc besmrtnosti.jpg" class="d-block w-100" >
                             </div>
                         </div>
                         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
@@ -62,6 +57,67 @@
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                         </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="interesovanja mt-4">
+            <h2 class="text-center pt-5 pb-5 text-uppercase">Nase preporuke</h2>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+                            <div class="carousel-inner">
+                                <div class="carousel-item slider-image active">
+                                    <div class="row poravnanje">
+                                        <div class="col-md-4">
+                                            <img src="./images/server/knjige/arhipelag za drugi zivot.jpg" class="margin-left" alt="">
+                                        </div>
+                                        <div class="col-md-4">
+                                            <img src="./images/server/knjige/ognjena kapija.jpg" alt="">
+                                        </div>
+                                        <div class="col-md-4">
+                                            <img src="./images/server/knjige/zlatni grad.jpg" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="carousel-item slider-image">
+                                    <div class="row poravnanje">
+                                        <div class="col-md-4">
+                                            <img src="./images/server/knjige/dani secanja.jpg" class="margin-left" alt="">
+                                        </div>
+                                        <div class="col-md-4">
+                                            <img src="./images/server/knjige/otkrivanje hilandara.jpg" alt="">
+                                        </div>
+                                        <div class="col-md-4">
+                                            <img src="./images/server/knjige/mesecari.jpg" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="carousel-item slider-image">
+                                    <div class="row poravnanje">
+                                        <div class="col-md-4">
+                                            <img src="./images/server/knjige/cudovisna dadilja.jpg" class="margin-left" alt="">
+                                        </div>
+                                        <div class="col-md-4">
+                                            <img src="./images/server/knjige/bogati otac siromasni otac.jpg" alt="">
+                                        </div>
+                                        <div class="col-md-4">
+                                            <img src="./images/server/knjige/medvedi na putu.jpg" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
